c185924b3adccb7116a7b5ff340fca07d98428f6
Wisman Nur <wismannura@gmail.com>
2020-10-19 11:35:18 +0700

    change env

--- api/configDB.js
+++ api/configDB.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 // for connection to DB
 const connection = mysql.createConnection({
-	host: process.env.dbhost,
-	user: process.env.dbuser,
-	password: process.env.dbpassword,
-	database: process.env.database,
+	host: process.env.DBHOST,
+	user: process.env.DBUSER,
+	password: process.env.DBPASSWORD,
+	database: process.env.DATABASE,
 });
 
 module.exports = connection;
